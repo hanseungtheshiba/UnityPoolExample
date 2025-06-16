aec5a0248552610c9bfb731ac6841c734d656812
TeacherShiba <dmetinn@gmail.com>
2025-06-17 02:44:10 +0900

    feat: 총알 발사 풀링 완성
    최종 수업 목표

--- Assets/Scripts/Player/BulletMove.cs
+++ Assets/Scripts/Player/BulletMove.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Pool;
 
 /// <summary>
 /// 총알을 움직이는 스크립트.
@@ -6,12 +7,26 @@
 public class BulletMove : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed = 10f;  
+    private float moveSpeed = 10f;    
+    private IObjectPool<GameObject> bulletPool;
+
+    private void OnEnable()
+    {
+        Invoke("Despawn", 2f);
+    }
 
     private void Update()
     {
         transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
     }
 
-    // TODO: 총알이 화면 밖으로 나가면 Pool에 돌아가게끔 만들어보자
+    private void Despawn()
+    {
+        bulletPool.Release(gameObject);
+    }
+
+    public void SetBulletPool(IObjectPool<GameObject> bulletPool)
+    {
+        this.bulletPool = bulletPool;
+    }
 }
--- Assets/Scripts/Player/BulletShooter.cs
+++ Assets/Scripts/Player/BulletShooter.cs
@@ -1,9 +1,57 @@
 ﻿using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// 총알 발사용 스크립트. 
 /// </summary>
 public class BulletShooter : MonoBehaviour
 {
-    // TODO : ObjectPool을 이용해 총알 생성을 구현해봅시다.
+    [SerializeField]
+    private GameObject bulletObject = null;
+
+    private IObjectPool<GameObject> bulletPool = null;
+    public IObjectPool<GameObject> BulletPool
+    {
+        get {
+            if(bulletPool == null)
+            {
+                bulletPool = new ObjectPool<GameObject>(CreateBullet, OnTakeFromPool, OnReturnedToPool);
+            }
+            return bulletPool;
+        }
+    }
+    
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        BulletPool.Get();
+    }
+
+    private GameObject CreateBullet()
+    {
+        GameObject newBullet = ResetBullet(Instantiate(bulletObject));
+        return newBullet;
+    }
+
+    private void OnTakeFromPool(GameObject bullet)
+    {
+        ResetBullet(bullet);
+    }
+
+    private void OnReturnedToPool(GameObject bullet) 
+    {
+        bullet.SetActive(false);
+    }
+
+    private GameObject ResetBullet(GameObject bullet)
+    {
+        BulletMove bulletMove = bullet.GetComponent<BulletMove>();
+        // newBullet의 BulletMove에 bulletPool을 세팅해준다.
+        bulletMove.SetBulletPool(bulletPool);
+        // 위치 초기화
+        bullet.transform.position = transform.position;
+        bullet.SetActive(true);
+
+        return bullet;
+    }
 }
