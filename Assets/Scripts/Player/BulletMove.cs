08d7c77e3b0dd5967b2f13b70e7f0426f460e0f7
TeacherShiba <dmetinn@gmail.com>
2025-06-17 02:07:21 +0900

    feat: 리소스/기본 기능 업로드
    플레이어 이동,배경 스크롤

---
+++ Assets/Scripts/Player/BulletMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 총알을 움직이는 스크립트.
+/// </summary>
+public class BulletMove : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10f;  
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+    }
+
+    // TODO: 총알이 화면 밖으로 나가면 Pool에 돌아가게끔 만들어보자
+}
---
+++ Assets/Scripts/Player/BulletShooter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 총알 발사용 스크립트. 
+/// </summary>
+public class BulletShooter : MonoBehaviour
+{
+    // TODO : ObjectPool을 이용해 총알 생성을 구현해봅시다.
+}
